99daac79ff6dacf5c7249ae7cefe9b85cb7aa128
KP <59389473+kpsaurus@users.noreply.github.com>
2021-07-23 22:31:21 +0530

    Update App.js
    Added Home component.

--- gallery_client/src/App.js
+++ gallery_client/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div>
       <h1 className='uk-text-center uk-text-success'>Gallery</h1>
+      <div className='uk-container uk-container-medium'>
+        <Home />
+        </div>
     </div>
   );
 }
