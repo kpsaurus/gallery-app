326eadb48b5ae4b2c39be06f133059e9a2e925f9
KP <59389473+kpsaurus@users.noreply.github.com>
2021-07-23 22:34:39 +0530

    Added margin bottom

--- gallery_client/src/components/File.js
+++ gallery_client/src/components/File.js
@@ -1,5 +1,5 @@
 function File({file}) {
-    return <div className="uk-tile uk-tile-muted uk-width-auto">
+    return <div className="uk-tile uk-tile-muted uk-width-auto uk-margin-bottom">
         <h3>{file}</h3>
     </div>
 }
--- gallery_client/src/components/Folder.js
+++ gallery_client/src/components/Folder.js
@@ -1,5 +1,5 @@
 function Folder({folder}) {
-    return <div className="uk-tile uk-tile-primary uk-width-auto">
+    return <div className="uk-tile uk-tile-primary uk-width-auto uk-margin-bottom">
         <h3>{folder}</h3>
     </div>
 }
