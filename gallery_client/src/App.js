5e7a623dd2cf82ff034c8629468c1962728ee322
KP <59389473+kpsaurus@users.noreply.github.com>
2021-07-23 20:17:34 +0530

    first commit

---
+++ gallery_client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className='uk-text-center uk-text-success'>Gallery</h1>
+    </div>
+  );
+}
+
+export default App;
