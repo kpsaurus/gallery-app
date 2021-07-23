11f0f003609d3f9f57ef2ace520d2bb6d2f85174
KP <59389473+kpsaurus@users.noreply.github.com>
2021-07-23 21:55:00 +0530

    Update views.py
    Added HomeView class for retrieving root directory items.

--- gallery_api/api/views.py
+++ gallery_api/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.core.files.storage import default_storage
+from django.conf import settings
 
-# Create your views here.
+
+class HomeView(APIView):
+    def get(self, request, format=None):
+        """
+        Return the folders and files inside root dir.
+        """
+        directories, files = default_storage.listdir('')
+        root_dir = {
+            'folders': directories,
+            'files': files
+        }
+
+        data = {'root_dir': root_dir}
+        return Response(data)
