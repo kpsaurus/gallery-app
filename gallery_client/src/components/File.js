d6c14d4104a570e4e297a2d91e78d7feddd19eda
KP <59389473+kpsaurus@users.noreply.github.com>
2021-07-25 18:57:59 +0530

    Auto width removed.

--- gallery_client/src/components/File.js
+++ gallery_client/src/components/File.js
@@ -1,5 +1,5 @@
 function File({file}) {
-    return <div className="uk-tile uk-tile-muted uk-width-auto uk-margin-bottom">
+    return <div className="uk-tile uk-tile-muted uk-margin-bottom">
         <h3>{file.name}</h3>
     </div>
 }
--- gallery_client/src/components/Folder.js
+++ gallery_client/src/components/Folder.js
@@ -1,5 +1,5 @@
 function Folder({folder}) {
-    return <div className="uk-tile uk-tile-primary uk-width-auto uk-margin-bottom">
+    return <div className="uk-tile uk-tile-primary uk-margin-bottom">
         <h3>{folder.name}</h3>
     </div>
 }
