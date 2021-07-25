1b94bbee67514a12c8536f9b0df16e1e19cd44a5
KP <59389473+kpsaurus@users.noreply.github.com>
2021-07-25 21:13:38 +0530

    Update File.js
    Added custom styles when mouse hover, mouse leave, click event occurs on the component.

--- gallery_client/src/components/File.js
+++ gallery_client/src/components/File.js
@@ -1,9 +1,22 @@
-import { breakWord } from "../utils/Styles";
+import { breakWordStyle, selectItemStyle,hoverInStyle,hoverOutStyle } from "../utils/Styles";
+import { useState } from 'react';
 function File({ file }) {
+    const [itemStyle, setItemStyle] = useState({})
+    const select = (e) => {
+        setItemStyle(selectItemStyle)
+    }
+    const mouseHover = (e) => {
+        setItemStyle(hoverInStyle)
+    }
+
+    const mouseLeave = (e) => {
+        setItemStyle(hoverOutStyle)
+    }
+
     return (
-        <div className="uk-tile uk-padding-small uk-tile-muted uk-margin-bottom">
+        <div onMouseEnter={mouseHover} onMouseLeave={mouseLeave} onClick={select} style={itemStyle} className="uk-tile uk-padding-small uk-tile-muted uk-margin-bottom">
             <span uk-icon="image"></span>
-            <h5 style={breakWord}>{file.name}</h5>
+            <h5 style={breakWordStyle}>{file.name}</h5>
         </div>
     )
 }
