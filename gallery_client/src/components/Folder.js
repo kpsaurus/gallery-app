17247de549f4f588eb656592209bfd744f692863
KP <59389473+kpsaurus@users.noreply.github.com>
2021-07-25 18:48:46 +0530

    Update Folder.js

--- gallery_client/src/components/Folder.js
+++ gallery_client/src/components/Folder.js
@@ -1,6 +1,6 @@
 function Folder({folder}) {
     return <div className="uk-tile uk-tile-primary uk-width-auto uk-margin-bottom">
-        <h3>{folder}</h3>
+        <h3>{folder.name}</h3>
     </div>
 }
 
