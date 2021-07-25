d7bd6f4d9372f4f74b8a17d7d9dd9aaa99dfb47e
KP <59389473+kpsaurus@users.noreply.github.com>
2021-07-25 20:21:14 +0530

    Update NewFolder.js
    Added some styling for the input focus / blur event.

--- gallery_client/src/components/NewFolder.js
+++ gallery_client/src/components/NewFolder.js
@@ -3,9 +3,30 @@ import React, { useState,useRef, useEffect } from 'react';
 function NewFolder({ folder }) {
 
     const [newFolderName, setNewFolderName] = useState(folder);
-    
+    const [inputStyle, setInputStyle] = useState({
+        'outline': '1px dashed blue',
+        'lineHeight': 3
+    })
+
+
     const renameFolder = (e) => {
         const newName = e.target.textContent
+
+        setInputStyle(
+            {
+                'outline': '1px dashed blue',
+                'lineHeight':3
+            }
+        )
+    }
+
+    const focusOut = (e) => {
+        setInputStyle(
+            {
+                'outline': '1px dashed black',
+                'lineHeight':3
+            }
+        )
     }
 
     const inputFolderName = useRef(null);
@@ -15,10 +36,13 @@ function NewFolder({ folder }) {
     }, [inputFolderName]);
 
 
-    return (<div className="uk-tile uk-tile-muted uk-margin-bottom">
-        <h3 autoFocus ref={inputFolderName} contentEditable="true" onInput={renameFolder} onBlur={renameFolder} suppressContentEditableWarning={true}>{newFolderName.name}</h3>
-        <button className="uk-button uk-button-default">Save</button>
-    </div>)
+    return (
+        <div>
+            <div className="uk-tile uk-tile-muted uk-margin-bottom">
+                <h5 style={inputStyle} autoFocus ref={inputFolderName} contentEditable="true" onInput={renameFolder} onBlur={focusOut}  suppressContentEditableWarning={true}>{newFolderName.name}</h5>
+                <button className="uk-button uk-button-default">Save</button>
+            </div>
+        </div>)
 }
 
 export default NewFolder;
