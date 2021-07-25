25b8d8948e6a9e23957c0072d707109d5a70d0ba
KP <59389473+kpsaurus@users.noreply.github.com>
2021-07-25 18:49:08 +0530

    Create Upload.js
    Upload component added for file upload

---
+++ gallery_client/src/components/Upload.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import client from '../axios/client';
+
+
+function Upload({ path }) {
+    
+    const [file,setFile]=useState(null)
+
+    const fileUpload = {
+        cursor: 'pointer'
+    }
+
+    async function uploadFile(formData) {
+        await client.put(
+            'object/',formData
+        ).then(res => {
+            if (res) {
+                console.log(res)
+            } else {
+                
+            }
+        }).catch(err => {
+            console.error(err)
+        })
+    }
+
+
+    const fileChange = (e) => {
+        setFile(e.target.files[0])
+        const formData = new FormData();
+        
+        formData.append('path', path);
+        formData.append('file', e.target.files[0]);
+        
+        uploadFile(formData)
+    }
+
+    return (
+        <div>
+            <label className='uk-tile uk-tile-primary' style={fileUpload}>
+                <input className='uk-hidden' type="file" name='file' onChange={fileChange}/>
+                <span uk-icon="icon: upload"></span> Custom Upload
+            </label>
+       </div>
+    )
+}
+
+export default Upload
