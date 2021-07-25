0d074a50df957f478524c4c6aeb0b7532895a1fa
KP <59389473+kpsaurus@users.noreply.github.com>
2021-07-25 20:48:33 +0530

    Create Styles.js
    Created a module for common styles used in the app.

---
+++ gallery_client/src/utils/Styles.js
@@ -0,0 +1,19 @@
+const newFolderFocusOutline = {
+    'outline': '1px dashed blue',
+    'lineHeight': 3
+}
+
+const newFolderBlurOutline = {
+    'outline': '1px dashed black',
+    'lineHeight':3
+}
+
+const breakWord = {
+        wordBreak: 'break-word'
+}
+
+export {
+    newFolderFocusOutline,
+    newFolderBlurOutline,
+    breakWord,
+}
