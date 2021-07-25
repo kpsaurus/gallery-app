ab6bbafc32b8a0e02645befa18a273ab5160d9c3
KP <59389473+kpsaurus@users.noreply.github.com>
2021-07-25 19:38:47 +0530

    Icon added for both file and folder component.

--- gallery_client/src/components/File.js
+++ gallery_client/src/components/File.js
@@ -1,7 +1,10 @@
 function File({file}) {
-    return <div className="uk-tile uk-tile-muted uk-margin-bottom">
-        <h3>{file.name}</h3>
-    </div>
+    return (
+        <div className="uk-tile uk-tile-muted uk-margin-bottom">
+            <span uk-icon="image"></span>
+            <h3>{file.name}</h3>
+        </div>
+    )
 }
 
 export default File;
--- gallery_client/src/components/Folder.js
+++ gallery_client/src/components/Folder.js
@@ -1,7 +1,9 @@
-function Folder({folder}) {
-    return <div className="uk-tile uk-tile-primary uk-margin-bottom">
-        <h3>{folder.name}</h3>
-    </div>
+function Folder({ folder }) {
+    return (
+        <div className="uk-tile uk-tile-muted uk-margin-bottom">
+            <span uk-icon="folder"></span>
+            <h3>{folder.name}</h3>
+    </div>)
 }
 
 export default Folder;
