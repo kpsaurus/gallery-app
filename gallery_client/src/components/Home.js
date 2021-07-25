b3a474c9e106c006d5f2cf22cb2e2c4a5f00c07c
KP <59389473+kpsaurus@users.noreply.github.com>
2021-07-25 19:45:51 +0530

    Update Home.js
    Imported NewFolder component. If the user clicks on the add folder button, this component will be rendered.

--- gallery_client/src/components/Home.js
+++ gallery_client/src/components/Home.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import client from '../axios/client';
 import Folder from './Folder';
+import NewFolder from './NewFolder';
 import File from './File';
 import Upload from './Upload';
 
 function Home() {
 
     const [items, setItem] = useState([])
+    const [newFolder, setNewFolder] = useState({})
     const [path, setPath] = useState('')
 
     async function fetchData() {
@@ -24,27 +26,45 @@ function Home() {
         })
     }
 
+    const addFolder = () => {
+        const newFolder = { 'name': 'New Folder' }
+        setNewFolder(newFolder)
+    }
+
     useEffect(() => {
         fetchData()
   },[]);
 
-    return (<div className="uk-grid uk-child-width-1-4@m uk-grid-match">
-        
-        {   items.folders ? (
-            items.folders.map((folder,index) => <div key={index}>
-                <Folder folder={folder} />
-                </div>
-            )) : ''
-        }
-        {   items.files ? (
-            items.files.map((file,index) => <div key={index}>
-                <File file={file} />
-                </div>
-            )) : ''
-        }
+    return (
+        <div>
+            <hr className="uk-divider-icon"></hr>
+            <div className="uk-flex uk-flex-between">
+            <p>Current path: {path ? path : '"/"'}</p>
+                <button className="uk-button uk-button-secondary" onClick={addFolder}>Add a new folder</button>
+            </div>
+            <hr></hr>
+            <div className="uk-grid uk-child-width-1-4@m uk-grid-match">
 
-        <Upload path={path} fetchData={fetchData} />
-    </div>
+            {
+                newFolder.name ? (<NewFolder folder={newFolder} />):''
+            }
+            
+            {   items.folders ? (
+                items.folders.map((folder,index) => <div key={index}>
+                    <Folder folder={folder} />
+                    </div>
+                )) : ''
+            }
+            {   items.files ? (
+                items.files.map((file,index) => <div key={index}>
+                    <File file={file} />
+                    </div>
+                )) : ''
+            }
+
+            <Upload path={path} fetchData={fetchData} />
+            </div>
+        </div>
     )
 }
 
