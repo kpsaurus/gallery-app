597f35850d6d8e48f680fb739e77ee1dd616fb8f
KP <59389473+kpsaurus@users.noreply.github.com>
2021-07-25 20:02:43 +0530

    Word break added for files and folders.

--- gallery_client/src/components/File.js
+++ gallery_client/src/components/File.js
@@ -1,8 +1,11 @@
-function File({file}) {
+function File({ file }) {
+    const breakWord = {
+        wordBreak: 'break-word'
+    }
     return (
         <div className="uk-tile uk-tile-muted uk-margin-bottom">
             <span uk-icon="image"></span>
-            <h3>{file.name}</h3>
+            <h3 style={breakWord}>{file.name}</h3>
         </div>
     )
 }
--- gallery_client/src/components/Folder.js
+++ gallery_client/src/components/Folder.js
@@ -1,8 +1,11 @@
 function Folder({ folder }) {
+    const breakWord = {
+        wordBreak: 'break-word'
+    }
     return (
         <div className="uk-tile uk-tile-muted uk-margin-bottom">
             <span uk-icon="folder"></span>
-            <h3>{folder.name}</h3>
+            <h3 style={breakWord}>{folder.name}</h3>
     </div>)
 }
 
