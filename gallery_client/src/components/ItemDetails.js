0e76c919eb41d70912228ecedc411bd5b0e53dfc
KP <59389473+kpsaurus@users.noreply.github.com>
2021-07-29 21:38:37 +0530

    Update ItemDetails.js
    Added delete api for selected item.

--- gallery_client/src/components/ItemDetails.js
+++ gallery_client/src/components/ItemDetails.js
@@ -1,4 +1,45 @@
-function ItemDetails({details}) {
+import UIkit from "uikit";
+import client
+from "../axios/client";
+function ItemDetails({ path, setPath, details }) {
+
+    async function deleteItem() {
+        await client.delete(
+            'object/', { 'data': { 'file':path } }
+        ).then(res => {
+            if (res) {
+                if (res.data.status === 'success') {
+                     UIkit.notification({
+                        message: 'Successfully deleted the file',
+                        status: 'success',
+                        pos: 'bottom-center',
+                        timeout: 3000
+                     });
+                    setPath('')
+                } else {
+                    const errors = res.data.errors
+                    for (let i = 0; i < errors.length; i++){
+                        for (let j = 0; j < errors[i]['errors'].length; j++){
+                            UIkit.notification({
+                                message: errors[i].field + ': ' + errors[i]['errors'][j],
+                                status: 'danger',
+                                pos: 'bottom-center',
+                                timeout: 3000
+                            });
+                        }
+                    }
+                }
+            }
+        })
+    }
+    
+    const delete_item = (e) => {
+        UIkit.modal.confirm('UIkit confirm!').then(function() {
+        deleteItem()
+    }, function () {
+    });
+    }
+
     return (<div>
         <div className="uk-grid uk-child-width-1-2@m">
             <div>
@@ -7,6 +48,9 @@ function ItemDetails({details}) {
             <div>
                 <h4>{details.name}</h4>
                 <h4>Size: {(details.size / 1024).toFixed(2)} KB</h4>
+                <div className="uk-margin-top">
+                    <button className="uk-button uk-button-danger" onClick={delete_item}><span uk-icon="icon: trash"></span> Delete</button>
+                </div>
             </div>
         </div>
     </div>
