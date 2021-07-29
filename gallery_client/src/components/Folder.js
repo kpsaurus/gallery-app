b28c02e9e3c212bb0a0355a8bf6fc80a2d29b618
KP <59389473+kpsaurus@users.noreply.github.com>
2021-07-29 21:49:32 +0530

    Update Folder.js
    On clicking the component, setPath method will be called.

--- gallery_client/src/components/Folder.js
+++ gallery_client/src/components/Folder.js
@@ -1,7 +1,13 @@
-import { breakWordStyle } from "../utils/Styles";
-function Folder({ folder }) {
+import { breakWordStyle} from "../utils/Styles";
+import { useState } from 'react';
+function Folder({ folder, setPath }) {
+
+    const select = (e) => {
+        setPath(folder.name)
+    }
+
     return (
-        <div className="uk-tile uk-padding-small uk-tile-muted uk-border-rounded uk-margin-bottom">
+        <div className="uk-tile pointer uk-padding-small uk-tile-muted uk-border-rounded uk-margin-bottom" onClick={select}>
             <span uk-icon="folder"></span>
             <h5 style={breakWordStyle}>{folder.name}</h5>
     </div>)
