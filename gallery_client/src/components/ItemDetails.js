e9c60e867d894f021048c739bbfc3cdd422c366b
KP <59389473+kpsaurus@users.noreply.github.com>
2021-07-29 21:06:37 +0530

    Create ItemDetails.js
    New component added for displaying the basic item details and displaying the image.

---
+++ gallery_client/src/components/ItemDetails.js
@@ -0,0 +1,17 @@
+function ItemDetails({details}) {
+    return (<div>
+        <div className="uk-grid uk-child-width-1-2@m">
+            <div>
+                <img src={details.url}></img>
+            </div>
+            <div>
+                <h4>{details.name}</h4>
+                <h4>Size: {(details.size / 1024).toFixed(2)} KB</h4>
+            </div>
+        </div>
+        
+    </div>
+    )
+}
+
+export default ItemDetails;
