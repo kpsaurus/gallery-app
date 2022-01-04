0a66e5c58d14f254ec03ec6c82629ed91b674c69
KP <59389473+kpsaurus@users.noreply.github.com>
2022-01-04 19:48:49 +0530

    Update App.js
    Imported PathProvider

--- gallery_client/src/App.js
+++ gallery_client/src/App.js
@@ -1,15 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './components/Home';
+import logo from "./logo.svg";
+import "./App.css";
+import { PathProvider } from "./context/Context";
+import Home from "./components/Home";
 
 function App() {
-  return (
-    <div>
-      <div className='uk-container uk-container-medium'>
-        <Home />
-        </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="uk-container uk-container-medium">
+				<PathProvider>
+					<Home />
+				</PathProvider>
+			</div>
+		</div>
+	);
 }
 
 export default App;
